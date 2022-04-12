5067ff755b5736fc49503e56f74fda9be28f2407
Md Mahfuzur Rahman <mahfuznow@gmail.com>
2022-04-12 16:57:43 +0600

    Demo Gallery Screen Added

--- app/src/main/java/com/mahfuznow/examplejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/mahfuznow/examplejetpackcompose/MainActivity.kt
@@ -3,13 +3,27 @@ package com.mahfuznow.examplejetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mahfuznow.examplejetpackcompose.ui.theme.ExampleJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +33,152 @@ class MainActivity : ComponentActivity() {
             ExampleJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainActivityScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ExampleJetpackComposeTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+            MainActivityScreen()
+        }
+    }
+}
+
+@Composable
+fun MainActivityScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "My Gallery",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+        MyGallery()
+    }
+}
+
+@Composable
+fun MyGallery() {
+
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+                .padding(vertical = 8.dp)
+        ) {
+            ImageCard(
+                painter = painterResource(id = R.drawable.wave),
+                contentDescription = "Cox's Bazaar, Bangladesh",
+                title = "Cox's Bazaar, Bangladesh",
+                modifier = Modifier
+                    .width(180.dp)
+            )
+            ImageCard(
+                painter = painterResource(id = R.drawable.leaf),
+                contentDescription = "Sundar ban, Bangladesh",
+                title = "Sundar ban, Bangladesh",
+                modifier = Modifier
+                    .width(180.dp)
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+                .padding(vertical = 8.dp)
+        ) {
+            ImageCard(
+                painter = painterResource(id = R.drawable.fire),
+                contentDescription = "Hill Camp Fire, Bangladesh",
+                title = "Hill Camp Fire, Bangladesh",
+                modifier = Modifier
+                    .width(180.dp)
+            )
+            ImageCard(
+                painter = painterResource(id = R.drawable.star),
+                contentDescription = "Space Stars, Bangladesh",
+                title = "Space Stars, Bangladesh",
+                modifier = Modifier
+                    .width(180.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 5.dp,
+        modifier = modifier
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300F
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Text(text = title, color = Color.White)
+            }
+            Box(
+                contentAlignment = Alignment.TopEnd,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(8.dp)
+            ) {
+                Image(
+                    painterResource(id = R.drawable.ic_baseline_favorite_border_24),
+                    contentDescription = "Favourite Icon"
+                )
+            }
+
+        }
     }
 }
